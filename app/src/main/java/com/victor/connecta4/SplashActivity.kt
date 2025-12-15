5c7cb46e53b9a1b4af8eb710ced36b9082aaf028
Victor Prieto <victorpg182006@gmail.com>
2025-12-15 17:21:27 +0100

    .

---
+++ app/src/main/java/com/victor/connecta4/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.victor.connecta4
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        // Esperar 3 segundos y luego ir a MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
